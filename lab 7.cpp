49d05638a7b5946a896c0cd7901503e66efa604b
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2024-12-16 12:30:01 +0500

    edited

---
+++ lab 7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"enter any number to run aa loop:";
+cin>>num;
+for( int i=0; i<=num;  i++){
+
+cout<<"iterations"<<i<<endl;
+
+}
+
+
+
+return 0;
+}
