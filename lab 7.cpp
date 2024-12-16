39ca3e3c0952b2e2a804f4c7e3a979fa388e5a5f
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2024-12-16 12:39:31 +0500

    edited c++

--- lab 7.cpp
+++ lab 7.cpp
@@ -5,9 +5,15 @@ int main()
 int num;
 cout<<"enter any number to run aa loop:";
 cin>>num;
+if(num>0)
+{
 for( int i=0; i<=num;  i++){
 
 cout<<"iterations"<<i<<endl;
+}
+}
+else{
+    cout<<"enter positive number"<<endl;
 
 }
 
