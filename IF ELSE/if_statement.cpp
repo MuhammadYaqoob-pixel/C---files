c1ba8b109f78685aca0f7b73709e3d34314804d1
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-01-07 19:05:56 +0500

    added

---
+++ IF ELSE/if_statement.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+// int money;
+// cout<<"enter money"<<endl;
+// cin>>money;
+
+// if(money>=100){
+
+// cout<<"coffee at strawbuck";
+// }
+// else
+//  cout<<"go to home";
+
+// There are 2 statements in if  but you didn’t use {  } , what happened?
+
+
+// int i=16;
+// if(i>15)
+// cout<<"16>15";
+
+// cout<<"good luck";
+
+// cout<<"i am not a if";
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
