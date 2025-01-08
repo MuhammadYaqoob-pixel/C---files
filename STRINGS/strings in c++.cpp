71deb9bf3ec3c43879fdfec3b28bec80b0616d14
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-01-08 20:56:17 +0500

    aded

---
+++ STRINGS/strings in c++.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	/*string firstname= "10";
+	string lastname="20";
+	//cout<<firstname+lastname;
+	string fulname=firstname+lastname;
+	//cout<<"fulname is"<<endl;
+	cout<<fulname;
+	*/
+	// int x=10;
+	// string y ="20";
+	// string z=x+y;
+
+	
+	
+	
+	
+ 	// string txt="MUHAMMAD YAQOOB";
+	// cout<<"length of string is:"<<txt.length();       
+	                                                                                        
+	//string mystring="MUHAMMAD YAQOOB";
+// cout<<"size of string is:"<<mystring.size();
+		
+
+//  cout<< "mystring[1]";
+//  	mystring [1] ='A';
+//  	cout<<mystring;
+
+//  string mystring ="hello";
+//  mystring[0]='j';
+//  cout<<mystring;
+
+//cout<<mystring [1];
+
+/*
+cout<<"my name is \\Muhammad Yaqoob\\:";
+*
+string fulname;
+cout<<"enter your fulname:";
+getline(cin, fulname);
+cout<<"your fulname is:" " "<<fulname<<endl;
+
+
+*/
+
+
+
+for(int i=1; i<=100; i++){
+
+	cout<<"sorry baby"<<endl;
+}
+
+	return 0;
+	
+}
