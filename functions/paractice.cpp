2e6b38ff8c64c264cfcfe197de3f39b6c1575e94
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-01-13 12:48:17 +0500

    add functions

---
+++ functions/paractice.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+// int sum();
+void sum()
+{
+int a, b;
+cout<<"enter ist value"<<endl;
+cin>>a;
+cout<<"enter 2nd value "<<endl;
+cin>>b;
+int c=a+b;
+cout<<"sum is:"<<c<<endl;
+
+
+
+}
+int main()
+{
+sum();
+sum();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sum();
+
+
+    return 0;
+}
+// int sum(){
+//     int a, b;
+//     cout<<"enter 1st value"<<endl;
+//     cin>>a;
+//     cout<<"enter 2nd value"<<endl;
+//     cin>>b;
+//     int c =a+b;
+//     cout<<"sum is"<<c<<endl;
+// }
