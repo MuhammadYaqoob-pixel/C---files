20b0f62d46b10cb5582128677ec5a4992560548d
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-01-20 14:09:08 +0500

    add

--- functions/output/loops in function.cpp
+++ functions/output/loops in function.cpp
@@ -1,17 +1,31 @@
 #include<iostream>
+#include<string>
 using namespace std;
-int inc(int i){
-static int count=0;
-count=count+i;
-return(count);
+void exam(int* i, int j)
+{
+
+j=j+5;
+*i=*i+j;
 
 
 }
 int main(){
-int i, j;
-for(i=0; i<=6; i++)
- j=inc(i);
- cout<<j;
+    int i=6; 
+    int j=10;
+    exam(&i, j);
+    cout<<i+j;
+// int inc(int i){
+// static int count=0;
+// count=count+i;
+// return(count);
+
+
+// }
+// int main(){
+// int i, j;
+// for(i=0; i<=6; i++)
+//  j=inc(i);
+//  cout<<j;
 
 
 
