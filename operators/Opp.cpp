e851cd087bb5e402fdf5fb31dc7a17a80e9b3bda
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-01-29 12:17:59 +0500

    added

---
+++ operators/Opp.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     int a = 6, b = 4;
+//     // Equal to operator
+// cout << "a == b is " << (a == b) << endl;
+//     // Greater than operator
+// cout << "a > b is " << (a > b) << endl;
+//     // Greater than or Equal to operator
+// cout << "a >= b is " << (a >= b) << endl;
+//     //  Lesser than operator
+// cout << "a < b is " << (a < b) << endl;
+//     // Lesser than or Equal to operator
+// cout << "a <= b is " << (a <= b) << endl;
+    // true    cout << "a != b is " << (a != b) << endl; 
+
+// int x=10; 
+// int y;
+// y=++x;
+// cout<<x<<endl<<y<<endl;
+// y=x++;
+// cout<<x<<endl<<y<<endl;
+
+
+
+// int a = 8, b = 3;
+//     // Addition operator
+// cout << "a + b = " << (a + b) << endl;
+//     // Subtraction operator
+// cout << "a - b = " << (a - b) << endl;
+//     // Multiplication operator
+// cout << "a * b = " << (a * b) << endl;
+//     // Division operator
+// cout << "a / b = " << (a / b) << endl;
+//     // Modulo operator
+// cout << "a % b = " << (a % b) << endl;
+
+
+
+
+// int a = 6, b = 4;
+//     // Equal to operator
+// cout << "a == b is " << (a == b) << endl;
+//     // Greater than operator
+// cout << "a > b is " << (a > b) << endl;
+//     // Greater than or Equal to operator
+// cout << "a >= b is " << (a >= b) << endl;
+//     //  Lesser than operator
+// cout << "a < b is " << (a < b) << endl;
+//     // Lesser than or Equal to operat
+// cout << "a <= b is " << (a <= b) << endl;
+//     // true    cout << "a != b is " << (a != b) << endl; 
+
+
+return 0;
+
+}
