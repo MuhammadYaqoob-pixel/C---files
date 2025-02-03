a60879cac84441ca5f9bfe6f800bd251eb8ca5a0
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-02-03 23:45:45 +0500

    added

---
+++ IF ELSE/Class_user.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+// int money;
+// cout<<"enter money you have"<<endl;
+// cin>>money;
+// if(money>=1000);
+
+//     cout<<"coffe at sukkon hotel ";
+
+
+//     cout<<"go to home;";
+
+
+
+int i = 16; 
+
+if (i > 15)  
+
+cout << "16 is greater than 15"; 
+
+                 cout << “ good luck”;
+ 
+
+cout << "I am Not in if"; 
+
+
+
+
+return 0;
+
+}
